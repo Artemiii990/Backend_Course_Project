8aa61580881637b32f0a3d1d7b3c44f5fc738078
Artemiii990 <pelmenosadchuk@gmail.com>
2026-05-01 15:36:28 +0200

    initial commit

---
+++ Migrations/20260410095116_FindCategories.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication1.Migrations
+{
+    /// <inheritdoc />
+    public partial class FindCategories : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Category",
+                table: "Products",
+                newName: "CategorySlug");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CategorySlug",
+                table: "Products",
+                newName: "Category");
+        }
+    }
+}
